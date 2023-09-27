30d2d14ef609a987a7a45b2ebbd6732731f76346
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-09-27 10:30:22 -0700

    tests: add unit tests for custom hooks

--- __tests__/unit/hooks/useCheckMounted.spec.ts
+++ __tests__/unit/hooks/useCheckMounted.spec.ts
@@ -2,28 +2,28 @@ import { renderHook } from '@testing-library/react'
 import { useCheckMounted } from '~/hooks/useCheckMounted'
 
 describe('useCheckMounted', () => {
-    it('returns true after component has mounted', () => {
-        let isMounted: boolean | undefined
-        const ComponentWithHook = () => {
-            isMounted = useCheckMounted()
-            return null
-        }
+  it('returns true after component has mounted', () => {
+    let isMounted: boolean | undefined
+    const ComponentWithHook = () => {
+      isMounted = useCheckMounted()
+      return null
+    }
 
-        const { unmount } = renderHook(() => ComponentWithHook())
+    const { unmount } = renderHook(() => ComponentWithHook())
 
-        // Ensure the hook is evaluated after the component has mounted
-        expect(isMounted).toBe(true)
-        // Clean up to avoid any potential side effects
-        unmount()
+    // Ensure the hook is evaluated after the component has mounted
+    expect(isMounted).toBe(true)
+    // Clean up to avoid any potential side effects
+    unmount()
+  })
+
+  it('returns false after component has unmounted', () => {
+    let isMounted: boolean | undefined
+    const { unmount } = renderHook(() => {
+      isMounted = useCheckMounted()
     })
 
-    it('returns false after component has unmounted', () => {
-        let isMounted: boolean | undefined
-        const { unmount } = renderHook(() => {
-            isMounted = useCheckMounted()
-        })
-
-        expect(isMounted).toBe(true)
-        unmount()
-    })
+    expect(isMounted).toBe(true)
+    unmount()
+  })
 })
