a3b88088d4450f825de42ca27ad1966d66f6eb0b
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-09-26 19:38:24 -0700

    style: add intro animation

--- src/components/Heroes/IntroHero.tsx
+++ src/components/Heroes/IntroHero.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
+
 const IntroHero = () => {
   return (
-    <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
+    <section className="Animation-riseUp opacity-0 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-4xl font-medium tracking-tight text-[#e3edf6] sm:text-7xl">
         Inspections{' '}
         <span className="relative whitespace-nowrap text-blue-600">
@@ -11,18 +12,30 @@ const IntroHero = () => {
             preserveAspectRatio="none"
             viewBox="0 0 418 42"
           >
-            <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
+            <path
+              className="squiggle"
+              d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
+            ></path>
           </svg>
           <span className="relative">made simple.</span>
           <br />
         </span>
         Streamline vehicle assessment with AI.
       </h1>
-      <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-graySecondary">
-        Scheduling in-person inspections and doing manual paperwork is so
-        outdated. WheeledAI automates the entire rideshare vehicle inspection
-        workflow so you can earn quicker.
-      </p>
+      <div className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-graySecondary">
+        <h4>
+          Scheduling in-person inspections and doing manual paperwork is so
+          outdated.
+        </h4>
+        <p>
+          <span className="text-blue-300/70">
+            Wheeled.AI automates the entire rideshare vehicle inspection
+            workflow
+          </span>
+          <br />
+          so you can earn quicker.
+        </p>
+      </div>
       <div className="mt-10 flex justify-center gap-x-6">
         <Link
           className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 border transition-all ease border-blue-300/70 text-blue-300 hover:bg-slate-700 hover:scale-[0.98] hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,12 @@ export default {
   theme: {
     extend: {
       backgroundColor: {
-        'themeDark': '#121c25',
-        'themeLight': '#F6F5F8',
-        'offWhite': '#EAE8ED',
+        themeDark: '#121c25',
+        themeLight: '#F6F5F8',
+        offWhite: '#EAE8ED',
       },
       borderColor: {
-        'themeBorder': 'hsla(0,0%,51%,0.2)',
+        themeBorder: 'hsla(0,0%,51%,0.2)',
       },
       textColor: {
         dark: 'rgb(15 23 42)',
