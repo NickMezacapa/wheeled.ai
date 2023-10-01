794519b2265708521ff8990c1a53be90c22e3b90
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-10-01 13:41:09 -0700

    style: remove bg gradient from UI theme

--- src/components/Theme/BackgroundGradient.tsx
+++ src/components/Theme/BackgroundGradient.tsx
@@ -2,7 +2,7 @@ import Container from './Container'
 
 const BackgroundGradient = () => {
   return (
-    <Container className='hidden sm:block'>
+    <Container className='hidden'>
       <div className='GradientBlur Animation-fadeAndScale AnimationContainer w-full overflow-hidden'>
         <div className='GradientBg h-full w-full overflow-hidden' />
       </div>
