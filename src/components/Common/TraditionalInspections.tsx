4d722066fa2383aa1c5ed180a60aad87882ef6d4
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2023-10-16 20:02:49 -0700

    build: continue trad inspections component

--- src/components/Common/TraditionalInspections.tsx
+++ src/components/Common/TraditionalInspections.tsx
@@ -9,13 +9,34 @@ const TraditionalInspections = () => {
     >
       <Container>
         <div className='mx-auto max-w-2xl lg:mx-0 lg:max-w-3xl'>
-          <h1 className='text-[#1d1d1f]'>Ride Share Inspection Requirements</h1>
-          <p>
-            Uber and Lyft require all drivers to pass a vehicle inspection
-            before they can start driving. This inspection is to ensure that
-            your vehicle is safe to drive and meets the minimum requirements for
-            rideshare driving.
+          <h1 className='text-2xl font-semibold text-[#1d1d1f]'>
+            Ride Share Inspection Requirements
+          </h1>
+          <p className='mt-14 w-full text-[#1d1d1fba] md:w-4/5'>
+            Vehicle inspection is a mandatory prerequisite for all Uber drivers.
+            While there are various methods to fulfill this compliance, the
+            conventional approach involves scheduling appointments at mechanic
+            shops, which often results in certification delays by several days.
           </p>
+          <p className='mt-4 w-full text-[#1d1d1fba] md:w-4/5'>
+            WheeledAI offers a cutting-edge and streamlined solution to fulfill
+            Uber&apos;s vehicle inspection mandate without the need for a trip
+            to a mechanic. You can effortlessly access our platform via any web
+            browser, where you can simply upload photos and video clips of your
+            vehicle from your driveway or any location that suits you. It&apos;s
+            that straightforward.
+          </p>
+          <ul className='ml-6 mt-12 list-disc font-semibold'>
+            <li className='mt-4 w-full text-[#1d1d1fba] md:w-4/5'>
+              <p>No more scheduling appointments or manual paperwork</p>
+            </li>
+            <li className='mt-4 w-full text-[#1d1d1fba] md:w-4/5'>
+              <p>Conduct the inspection at your convenience</p>
+            </li>
+            <li className='mt-4 w-full text-[#1d1d1fba] md:w-4/5'>
+              <p>Receive your certification on the same day</p>
+            </li>
+          </ul>
         </div>
       </Container>
     </section>
